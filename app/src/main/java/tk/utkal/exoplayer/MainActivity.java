a38d0fa441e01367d0f033504255ee1f98d2a054
upradhan <urpradhan@gmail.com>
2018-11-15 18:50:29 +0530

    Added 1st music service implementation

--- app/src/main/java/tk/utkal/exoplayer/MainActivity.java
+++ app/src/main/java/tk/utkal/exoplayer/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.media.AudioManager;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,13 +12,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.MimeTypeMap;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -27,34 +25,14 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
-import com.google.android.exoplayer2.C;
-import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.DefaultRenderersFactory;
-import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.Player;
-import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.ext.cast.CastPlayer;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.hls.HlsMediaSource;
-import com.google.android.exoplayer2.source.smoothstreaming.DefaultSsChunkSource;
-import com.google.android.exoplayer2.source.smoothstreaming.SsChunkSource;
-import com.google.android.exoplayer2.source.smoothstreaming.SsMediaSource;
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelection;
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.MimeTypes;
-import com.google.android.exoplayer2.util.Util;
 import com.google.android.gms.cast.MediaInfo;
 import com.google.android.gms.cast.MediaMetadata;
 import com.google.android.gms.cast.MediaQueueItem;
 import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.google.android.gms.cast.framework.CastContext;
-import com.google.android.gms.cast.framework.CastState;
-import com.google.android.gms.cast.framework.CastStateListener;
 import com.google.android.gms.common.images.WebImage;
 
 import org.json.JSONArray;
@@ -63,21 +41,17 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT;
-import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK;
 import static com.google.android.gms.cast.framework.CastState.CONNECTED;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
-    AudioManager audioManager;
-    AudioManager.OnAudioFocusChangeListener afChangeListener;
+
     ListView listView;
     ListViewAdaptor listViewAdaptor;
     ProgressDialog mProgressDialog;
     int nCurrentStationId;
     private ArrayList<RadioStation> radioStations = new ArrayList<RadioStation>();
-    private SimpleExoPlayer player;
+
     private CastContext castContext;
     private CastPlayer castPlayer;
 
@@ -87,37 +61,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Create and initialize the player
-        createAndInitializePlayer();
+        createAndInitializeCastWidget();
 
         listView = (ListView) findViewById(R.id.listView);
-
 
         mProgressDialog = new ProgressDialog(MainActivity.this);
         mProgressDialog.setCancelable(false);
         mProgressDialog.setTitle("");
         mProgressDialog.setMessage("Please wait ...");
         mProgressDialog.show();
-
-        audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
-        afChangeListener = new AudioManager.OnAudioFocusChangeListener() {
-            public void onAudioFocusChange(int focusChange) {
-                if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
-                    // Permanent loss of audio focus
-                    player.stop();
-                } else if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT) {
-                    // Pause playback
-                    player.setPlayWhenReady(false);
-                } else // Lower the volume, keep playing
-                    if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
-                        player.setVolume((float) 0.5);
-                    } else if (focusChange == audioManager.AUDIOFOCUS_GAIN) {
-                        // Your app has been granted audio focus again
-                        // Raise volume to normal, restart playback if necessary
-                        player.setPlayWhenReady(true);
-                    }
-            }
-        };
-
 
         RequestQueue queue = Volley.newRequestQueue(this);
 
@@ -158,7 +110,12 @@ public class MainActivity extends AppCompatActivity {
                         RadioStation station = new RadioStation(name, tag, low, high, thumb, web, languages, genres);
                         radioStations.add(station);
                     }
+
+                    if (mProgressDialog != null && mProgressDialog.isShowing())
+                        mProgressDialog.dismiss();
+
                     processData();
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -195,40 +152,39 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void createAndInitializePlayer() {
-
-        TrackSelection.Factory adaptiveTrackSelectionFactory = new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
-
-        player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this),
-                new DefaultTrackSelector(adaptiveTrackSelectionFactory),
-                new DefaultLoadControl());
+    private void createAndInitializeCastWidget() {
 
         castContext = CastContext.getSharedInstance(this);
         castPlayer = new CastPlayer(castContext);
         castPlayer.setSessionAvailabilityListener(new CastPlayer.SessionAvailabilityListener() {
             @Override
             public void onCastSessionAvailable() {
-                Toast.makeText(MainActivity.this, "Cast session available", Toast.LENGTH_SHORT).show();
-                player.stop(true);
+                StopService();
                 setCurrentCastStation();
             }
 
             @Override
             public void onCastSessionUnavailable() {
-                Toast.makeText(MainActivity.this, "Cast session unavailable", Toast.LENGTH_SHORT).show();
                 castPlayer.stop();
-                preparePlayer();
+                StartService();
             }
         });
     }
 
-    private static String getMimeType(String url) {
-        String type = null;
-        String extension = MimeTypeMap.getFileExtensionFromUrl(url);
-        if (extension != null) {
-            type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
-        }
-        return type;
+    private void StartService() {
+        Intent intent = new Intent(MainActivity.this, MusicPlayerService.class);
+
+        String url = radioStations.get(nCurrentStationId).getHighUrl();
+        if(url.isEmpty())
+            url = radioStations.get(nCurrentStationId).getLowUrl();
+
+        intent.putExtra("curStationId", url);
+        startService(intent);
+    }
+
+    private void StopService() {
+        Intent intent = new Intent(MainActivity.this, MusicPlayerService.class);
+        stopService(intent);
     }
 
     private void setCurrentCastStation() {
@@ -270,58 +226,13 @@ public class MainActivity extends AppCompatActivity {
         castPlayer.loadItems(mediaItems, 0, 0, Player.REPEAT_MODE_OFF);
     }
 
-    private MediaSource buildMediaSource(Uri uri) {
-
-        DataSource.Factory dataSourceFactory = new DefaultHttpDataSourceFactory("ua", BANDWIDTH_METER);
-
-        int type = Util.inferContentType(uri);
-
-        switch (type) {
-            case C.TYPE_SS:
-                SsChunkSource.Factory ssChunkSourceFactory = new DefaultSsChunkSource.Factory(dataSourceFactory);
-                return new SsMediaSource.Factory(ssChunkSourceFactory, dataSourceFactory).createMediaSource(uri);
-
-            case C.TYPE_HLS:
-                return new HlsMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
-
-            case C.TYPE_OTHER:
-                return new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
-
-            default:
-                throw new IllegalStateException("Unsupported type: " + type);
-        }
-    }
-
-    private void preparePlayer() {
-        if (castContext.getCastState() == CONNECTED) {
-            setCurrentCastStation();
-            return;
-        }
-
-        String url = radioStations.get(nCurrentStationId).getHighUrl();
-        if(url.isEmpty())
-            url = radioStations.get(nCurrentStationId).getLowUrl();
-
-        Uri uri = Uri.parse(url);
-
-        MediaSource mediaSource = buildMediaSource(uri);
-        player.prepare(mediaSource, true, false);
-    }
-
-    private void releasePlayer() {
-        if (player != null) {
-            player.release();
-            player = null;
-        }
-        if (castPlayer != null) {
-            castPlayer.release();
-        }
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        releasePlayer();
+        StopService();
+        if (castPlayer != null) {
+            castPlayer.release();
+        }
     }
 
     @Override
@@ -338,9 +249,9 @@ public class MainActivity extends AppCompatActivity {
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        StopService();
                         finish();
                     }
-
                 })
                 .setNegativeButton("No", null)
                 .show();
@@ -353,23 +264,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 view.setSelected(true);
-
                 nCurrentStationId = i;
-                preparePlayer();
 
-                int res = audioManager.requestAudioFocus(afChangeListener, AudioManager.STREAM_MUSIC, // Music streaming
-                        AudioManager.AUDIOFOCUS_GAIN); // Permanent focus
-                if (res == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                    // Play the audio
-                    player.setPlayWhenReady(true);
+                if (castContext.getCastState() == CONNECTED) {
+                    setCurrentCastStation();
+                }
+                else {
+                    StartService();
                 }
             }
         });
-
-        if (mProgressDialog != null && mProgressDialog.isShowing())
-            mProgressDialog.dismiss();
     }
-
 
     class ListViewAdaptor extends BaseAdapter {
 
